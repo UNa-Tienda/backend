653c44f1ae4be23c3645f98d925314c05351d71e
Rhiino7 <36159485+Rhiino7@users.noreply.github.com>
2020-10-04 22:03:40 -0500

    jwt token WIP

--- src/main/java/com/SEII/configuration/MvcConfig.java
+++ src/main/java/com/SEII/configuration/MvcConfig.java
@@ -1,16 +1,17 @@
-package com.SEII.configuration;
+// package com.SEII.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class MvcConfig implements WebMvcConfigurer{
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/home").setViewName("home");
-    registry.addViewController("/").setViewName("home");
-    registry.addViewController("/hello").setViewName("hello");
-    registry.addViewController("/login").setViewName("login");
-  }
-}
+// @Configuration
+// public class MvcConfig implements WebMvcConfigurer{
+//   @Override
+//   public void addViewControllers(ViewControllerRegistry registry) {
+//     registry.addViewController("/home").setViewName("home");
+//     registry.addViewController("/").setViewName("home");
+//     registry.addViewController("/hello").setViewName("hello");
+//     registry.addViewController("/goodbye").setViewName("goodbye");
+//     registry.addViewController("/login").setViewName("login");
+//   }
+// }
