91b0d9d28474fd76f5968dcaf528d49646b39dc4
KaterineMoon <dkgarciad@unal.edu.co>
2020-10-06 07:26:43 -0500

    Migrations version fixed

---
+++ src/main/resources/db/migrations/postgres/V4__insert_person_data.sql
@@ -0,0 +1,5 @@
+-- INSERT DATA
+INSERT INTO public.person (id, name, username, email, password)
+VALUES
+    (1, 'Person 1', 'test1', 'test@gmail.com', 'qwertyuiop'),
+    (2, 'Person 2', 'test2', 'test2@gmail.com', 'qwertyuiop');
