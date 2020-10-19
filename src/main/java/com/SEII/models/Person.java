3ea4a04691d1d85635d410acccd8c2e6741739e4
Nicolas Sanchez <nisanchezva@unal.edu.co>
2020-10-19 17:37:43 -0500

    Transición a las nuevas tablas

--- src/main/java/com/SEII/models/Person.java
+++ src/main/java/com/SEII/models/Person.java
@@ -80,6 +80,9 @@ public class Person {
         this.location = location;
 
     }
+
+
+    
     
     @Override
     public String toString() {
@@ -100,6 +103,70 @@ public class Person {
         builder.append(", ");
 
         return builder.toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
 }
