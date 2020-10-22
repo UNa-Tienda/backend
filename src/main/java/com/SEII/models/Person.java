53aca4602476580ec605b6eb81f214edd6ca0348
Adrian-ACI <aacanacuani@unal.edu.co>
2020-10-21 20:09:55 -0500

    update person

--- src/main/java/com/SEII/models/Person.java
+++ src/main/java/com/SEII/models/Person.java
@@ -3,23 +3,13 @@ package com.SEII.models;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 
-@Table(name = "person")
+@Table(name = "person",uniqueConstraints={@UniqueConstraint(columnNames={"username"})})
 
 public class Person {
 
