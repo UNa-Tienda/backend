25028c29ded63e770828b2c0e0aa71810b610e1b
Nicolas Sanchez <nisanchezva@unal.edu.co>
2020-11-24 21:40:35 -0500

    Correción de algunas librerias importadas

--- src/test/java/com/ingesoft2/postTests/postTests.java
+++ src/test/java/com/ingesoft2/postTests/postTests.java
@@ -1,9 +1,7 @@
 package com.ingesoft2.postTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 
 import com.ingesoft2.models.PersonDTO;
 import com.ingesoft2.models.Post;
