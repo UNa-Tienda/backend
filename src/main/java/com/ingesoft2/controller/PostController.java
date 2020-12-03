8cd796e3a1ee2688a6e119bd1d335907160d92f6
Nicolas Sanchez <nisanchezva@unal.edu.co>
2020-12-03 08:57:20 -0500

    Funcionalidad de subir fotos terminada

--- src/main/java/com/ingesoft2/controller/PostController.java
+++ src/main/java/com/ingesoft2/controller/PostController.java
@@ -13,7 +13,6 @@ import com.ingesoft2.models.CartshopItem;
 import com.ingesoft2.models.Category;
 import com.ingesoft2.models.PersonDTO;
 import com.ingesoft2.models.Post;
-import com.ingesoft2.pojo.CreatePostImagePOJO;
 import com.ingesoft2.pojo.CreatePostPOJO;
 import com.ingesoft2.pojo.UpdatePostPOJO;
 import com.ingesoft2.services.CartshopItemService;
@@ -34,6 +33,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.image.BufferedImage;
 
@@ -46,6 +46,7 @@ public class PostController {
     private PostService postService;
     private CategoryService categoryService;
     private CartshopItemService cartshopItemService;
+    public String imageLink;
 
     @Autowired
     public PostController(PersonService personService, PostService postService, CategoryService categoryService, CartshopItemService cartshopItemService){
@@ -53,6 +54,7 @@ public class PostController {
         this.postService = postService;
         this.categoryService = categoryService;
         this.cartshopItemService = cartshopItemService;
+        imageLink = "";
     }
 
 
@@ -78,98 +80,19 @@ public class PostController {
     }
 
 
-    @PostMapping("/pruebas")
-    public String postImage2() {
+    @PostMapping("/add-image")
+    public void createNewObjectWithImage(@RequestParam(value = "file", required = true) MultipartFile file)
+            throws IOException {
+
         String api = "https://api.imgur.com/3/upload";        
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Client-ID 79ae804643cffff");
-        headers.set("Content-Disposition", "form-data;");
-
-        File gatito = new File("C:\\Users\\Equipo\\Downloads\\gatitoPrueba.png");
-        String filename = gatito.getName();
-
-
-
-            byte[] res = new byte[0];
-        try {
-            BufferedImage image = ImageIO.read(gatito);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", baos);
-            res = baos.toByteArray();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        ByteArrayResource contentAsResource = new ByteArrayResource(res){
-            @Override
-            public String getFilename(){
-                return filename;
-            }
-        };
-
-
-        MultiValueMap<String, Object> formData = new LinkedMultiValueMap<>();
-        formData.add("image", contentAsResource);
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(formData, headers);
-
-        ResponseEntity<String> result = restTemplate.exchange(api, HttpMethod.POST,requestEntity,String.class);
-
-        String link = result.getBody();
-        int inicio = link.indexOf("https");
-        int fin = link.lastIndexOf("png");
-        link = link.substring(inicio, fin+3); // le sumo 3 ya que el indice fin apunta a la "p" y por que el metodo substring usa n-1.
-        return link;
-    }
-
-    @PostMapping("/add/{categoryId}") /*
-                                       * Aqui tocaria ver como se saca la informaciòn del token para poner la ID en
-                                       * lugar de ese "1" que esta fijo
-                                       */
-    public ResponseEntity<Void> addPost(@PathVariable Integer categoryId, @RequestBody CreatePostImagePOJO post) {
-
-        if (post != null) {
-            String username = SecurityContextHolder.getContext().getAuthentication().getName();
-            PersonDTO person = personService.findByUsername(username);
-            Category category = categoryService.getCategory(categoryId);
-            if (person == null) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-            Post post2 = new Post();
-            post2.setTitle(post.getTitle());
-            post2.setProductName(post.getProductName());
-            post2.setDescription(post.getDescription());
-            post2.setPrice(post.getPrice());
-            post2.setStock(post.getStock());
-            
-
-            post2.setSellerId(person);
-            post2.setCategoryId(category);
-
-            //Sección en la que se guarda la imagen en imgur y se obtiene el link
-
-            String api = "https://api.imgur.com/3/upload";        
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
             headers.set("Authorization", "Client-ID 79ae804643cffff");//Datos para acceder a la API
             headers.set("Content-Disposition", "form-data;");
             
-            File imagen = post.getImage();
-            String filename = imagen.getName();
+            String filename = file.getOriginalFilename();
 
-            //Pasando la imagen a un archivo binario para poder subirlo a imgur
-            byte[] res = new byte[0];
-            try {
-                BufferedImage image = ImageIO.read(imagen);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ImageIO.write(image, "png", baos);
-                res = baos.toByteArray();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            ByteArrayResource contentAsResource = new ByteArrayResource(res){
+            ByteArrayResource contentAsResource = new ByteArrayResource(file.getBytes()){
                 @Override
                 public String getFilename(){
                     return filename;
@@ -188,10 +111,38 @@ public class PostController {
             String link = result.getBody();
             int inicio = link.indexOf("https");
             int fin = link.lastIndexOf("png"); //Si la imagen no es png podriamos tener problemas D:
-            link = link.substring(inicio, fin+3); // le sumo 3 ya que el indice fin apunta a la "p" y por que el metodo substring usa n-1.
+            link = link.substring(inicio, fin+3);
+            this.imageLink = link;
+    }
+        
+       
+    
 
-            //Fin de la sección de imgur
-            post2.setImage(link);
+    @PostMapping("/add/{categoryId}") /*
+                                       * Aqui tocaria ver como se saca la informaciòn del token para poner la ID en
+                                       * lugar de ese "1" que esta fijo
+                                       */
+    public ResponseEntity<Void> addPost(@PathVariable Integer categoryId, @RequestBody CreatePostPOJO post)
+            throws IOException {
+
+        if (post != null) {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            PersonDTO person = personService.findByUsername(username);
+            Category category = categoryService.getCategory(categoryId);
+            if (person == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            Post post2 = new Post();
+            post2.setTitle(post.getTitle());
+            post2.setProductName(post.getProductName());
+            post2.setDescription(post.getDescription());
+            post2.setPrice(post.getPrice());
+            post2.setStock(post.getStock());
+            
+
+            post2.setSellerId(person);
+            post2.setCategoryId(category);
+            post2.setImage(this.imageLink);
             postService.insert(post2);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } else {
